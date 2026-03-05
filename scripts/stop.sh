9ce4bda0177cd77b6ad0bc44f32e46502974fd35
Wilton Gonçalves <wilton.goncalves@fbter.org.br>
2026-03-05 08:14:55 -0300

    Exclusão de script de automação para Windows e Linux

--- scripts/stop.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-echo "Parando containers..."
-podman stop processo-selecao-frontend processo-selecao-backend processo-selecao-sqlserver 2>/dev/null
-
-echo "Removendo containers..."
-podman rm processo-selecao-frontend processo-selecao-backend processo-selecao-sqlserver 2>/dev/null
-
-echo "Ambiente parado."
