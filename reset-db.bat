aa2d66b5efeba0e77b61a3c1fb8b152c2a33c737
Wilton Gonçalves <wilton.goncalves@fbter.org.br>
2026-03-31 08:45:58 -0300

    Troca de ProcessoSelecao por ProcessoSelecaoDb

--- reset-db.bat
+++ reset-db.bat
@@ -6,31 +6,28 @@ echo   Reset Banco de Dados - ProcessoSelecao
 echo ========================================
 echo.
 
-:: Carregar variáveis do .env
-for /f "usebackq tokens=*" %%a in ("%CD%\.env") do set "%%a"
-
 echo [1/2] Resetando IDENTITY da tabela ProcessosSelecao...
 podman exec processo-selecao-sqlserver /opt/mssql-tools18/bin/sqlcmd ^
   -S localhost -U sa -P "Processo@123" -C ^
-  -Q "USE ProcessoSelecao; DELETE FROM ProcessosSelecao; DBCC CHECKIDENT ('ProcessosSelecao', RESEED, 0);"
+  -Q "USE ProcessoSelecaoDb; DELETE FROM ProcessosSelecao; DBCC CHECKIDENT ('ProcessosSelecao', RESEED, 0);"
 
 echo.
 echo [2/2] Resetando IDENTITY das outras tabelas...
 podman exec processo-selecao-sqlserver /opt/mssql-tools18/bin/sqlcmd ^
   -S localhost -U sa -P "Processo@123" -C ^
-  -Q "USE ProcessoSelecao; DELETE FROM Candidatos; DBCC CHECKIDENT ('Candidatos', RESEED, 0);"
+  -Q "USE ProcessoSelecaoDb; DELETE FROM Candidatos; DBCC CHECKIDENT ('Candidatos', RESEED, 0);"
 
 podman exec processo-selecao-sqlserver /opt/mssql-tools18/bin/sqlcmd ^
   -S localhost -U sa -P "Processo@123" -C ^
-  -Q "USE ProcessoSelecao; DELETE FROM Avaliadores; DBCC CHECKIDENT ('Avaliadores', RESEED, 0);"
+  -Q "USE ProcessoSelecaoDb; DELETE FROM Avaliadores; DBCC CHECKIDENT ('Avaliadores', RESEED, 0);"
 
 podman exec processo-selecao-sqlserver /opt/mssql-tools18/bin/sqlcmd ^
   -S localhost -U sa -P "Processo@123" -C ^
-  -Q "USE ProcessoSelecao; DELETE FROM Documentos; DBCC CHECKIDENT ('Documentos', RESEED, 0);"
+  -Q "USE ProcessoSelecaoDb; DELETE FROM Documentos; DBCC CHECKIDENT ('Documentos', RESEED, 0);"
 
 podman exec processo-selecao-sqlserver /opt/mssql-tools18/bin/sqlcmd ^
   -S localhost -U sa -P "Processo@123" -C ^
-  -Q "USE ProcessoSelecao; DELETE FROM Baremas; DBCC CHECKIDENT ('Baremas', RESEED, 0);"
+  -Q "USE ProcessoSelecaoDb; DELETE FROM Baremas; DBCC CHECKIDENT ('Baremas', RESEED, 0);"
 
 echo.
 echo ========================================
