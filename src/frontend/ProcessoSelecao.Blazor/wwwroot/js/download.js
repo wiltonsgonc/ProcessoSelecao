3657a849a2ccddba667aa1409631830fcdc784e2
Wilton Gonçalves <wilton.goncalves@fbter.org.br>
2026-07-22 14:40:19 -0300

    feat: página de login do candidato com estilo e download via JS

---
+++ src/frontend/ProcessoSelecao.Blazor/wwwroot/js/download.js
@@ -0,0 +1,16 @@
+window.BlazorDownloadFile = (fileName, base64) => {
+    const byteCharacters = atob(base64);
+    const byteNumbers = new Array(byteCharacters.length);
+    for (let i = 0; i < byteCharacters.length; i++)
+        byteNumbers[i] = byteCharacters.charCodeAt(i);
+    const byteArray = new Uint8Array(byteNumbers);
+    const blob = new Blob([byteArray], { type: 'application/octet-stream' });
+    const url = URL.createObjectURL(blob);
+    const a = document.createElement('a');
+    a.href = url;
+    a.download = fileName;
+    document.body.appendChild(a);
+    a.click();
+    document.body.removeChild(a);
+    URL.revokeObjectURL(url);
+};
