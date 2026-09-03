899b5df2670c4b6bce25783b8dcc50dd887a743b
Wilton Gonçalves <wilton.goncalves@fbter.org.br>
2026-09-03 14:17:30 -0300

    test(blazor): ajustar teste para modo Development
    Mock de IWebHostEnvironment como Development

--- src/frontend/ProcessoSelecao.Blazor.Tests/Components/AvaliadorAvaliacaoTests.razor.cs
+++ src/frontend/ProcessoSelecao.Blazor.Tests/Components/AvaliadorAvaliacaoTests.razor.cs
@@ -1,6 +1,7 @@
 using Bunit;
 using FluentAssertions;
 using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.JSInterop;
 using Moq;
@@ -12,17 +13,21 @@ namespace ProcessoSelecao.Blazor.Tests.Components;
 public class AvaliadorAvaliacaoTests : TestContext
 {
     [Fact]
-    public void ShouldRedirectToLoginWhenNoToken()
+    public void ShouldNotRedirectInDevelopmentMode()
     {
         var mockJS = new Mock<IJSRuntime>();
         mockJS.Setup(x => x.InvokeAsync<string>("sessionStorage.getItem", It.IsAny<object[]>()))
             .ReturnsAsync("");
         Services.AddSingleton(mockJS.Object);
 
+        var mockEnv = new Mock<IWebHostEnvironment>();
+        mockEnv.Setup(e => e.EnvironmentName).Returns("Development");
+        Services.AddSingleton(mockEnv.Object);
+
         var navMan = Services.GetRequiredService<NavigationManager>();
 
         RenderComponent<AvaliadorAvaliacao>(parameters => parameters.Add(p => p.baremaId, 1));
 
-        navMan.Uri.Should().Contain("/avaliador/login");
+        navMan.Uri.Should().NotContain("/avaliador/login");
     }
 }
